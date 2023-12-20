d0a08a1ab00f0a4786a80c5940c1d218c1f19089
Alejandro Velasquez Dev <92229666+VelzckC0D3@users.noreply.github.com>
2023-12-20 12:06:47 -0500

    Ui Update

--- components/Cars/index.tsx
+++ components/Cars/index.tsx
@@ -24,7 +24,9 @@ function CarDisplay() {
               <h3>{car.model}</h3>
               <p>{car.description}</p>
 
-              <Button onClick={() => handleOpenModal(car)}>Más información</Button>
+              <Button className={classes.modalButton} onClick={() => handleOpenModal(car)}>
+                Más información
+              </Button>
             </div>
             {/* Add more car details as needed */}
           </div>
@@ -33,9 +35,20 @@ function CarDisplay() {
         <p>No cars available.</p>
       )}
 
-      <Modal opened={opened} onClose={close} title={selectedCar?.model}>
+      <Modal size={1500} opened={opened} onClose={close} title={selectedCar?.model}>
         {/* Display the selected car details here */}
-        <p>{selectedCar?.description}</p>
+        <div className={classes.modalImageCont}>
+          {' '}
+          <img src={selectedCar?.images[0]} alt="" />{' '}
+          <img src={selectedCar?.images[1]} alt="" />{' '}
+          <img src={selectedCar?.images[2]} alt="" />{' '}
+        </div>
+        <p className={classes.modalText}>{selectedCar?.description}</p>
+        <div className={classes.modalInfoCont}>
+          <p className={classes.modalInfoT}> <span> Precio: </span>  ${selectedCar.price.toLocaleString()}</p>
+          <p className={classes.modalInfoT}> <span> Modelo:  </span> {selectedCar.year}</p>
+          <p className={classes.modalInfoT}> <span> Color:  </span> {selectedCar.color}</p>
+        </div>
         {/* You can add more details about the selected car here */}
       </Modal>
     </Box>
