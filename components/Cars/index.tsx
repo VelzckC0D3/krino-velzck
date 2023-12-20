9057189c971c92a7f556aad1097e13b7fe3f9ca2
Alejandro Velasquez Dev <92229666+VelzckC0D3@users.noreply.github.com>
2023-12-20 13:45:09 -0500

    UI Update
    - Actualizada la vista del Modal

--- components/Cars/index.tsx
+++ components/Cars/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+/* eslint-disable @next/next/no-img-element */
 import React, { useState } from 'react';
 import { useDisclosure } from '@mantine/hooks';
 import { Modal, Button, Box } from '@mantine/core';
@@ -14,40 +16,82 @@ function CarDisplay() {
     open();
   };
 
+  const renderCars = () => {
+    // Check if there are more than 5 cars
+    if (displayCars.length > 5) {
+      // If yes, return only the last car
+      const lastCar = displayCars[displayCars.length - 1];
+      return renderCarItem(lastCar, displayCars.length - 1);
+    } else {
+      // If no, return all cars
+      return displayCars.map((car, index) => renderCarItem(car, index));
+    }
+  };
+
+  const renderCarItem = (car, index) => (
+    <div key={index} className={classes.carItem}>
+      <img src={car?.images[1]} alt={car.model} className={classes.carImg} />
+      <div className={classes.carInfo}>
+        <h3>{car.model}</h3>
+        <p>{car.description}</p>
+
+        <Button className={classes.modalButton} onClick={() => handleOpenModal(car)}>
+          Más información
+        </Button>
+      </div>
+      {/* Add more car details as needed */}
+    </div>
+  );
+
   return (
     <Box className={classes.box}>
       {displayCars.length > 0 ? (
-        displayCars.map((car, index) => (
-          <div key={index} className={classes.carItem}>
-            <img src={car.images[1]} alt={car.model} className={classes.carImg} />
-            <div className={classes.carInfo}>
-              <h3>{car.model}</h3>
-              <p>{car.description}</p>
-
-              <Button className={classes.modalButton} onClick={() => handleOpenModal(car)}>
-                Más información
-              </Button>
-            </div>
-            {/* Add more car details as needed */}
-          </div>
-        ))
+        renderCars()
       ) : (
-        <p>No cars available.</p>
+        <div className={classes.temporalBgCont}>
+          <div className={classes.temporalTitle}>
+            <h2>Buscador De Vehiculos Land Rover</h2>
+            <p>
+              Describe el automovil que buscas o escribe 'vehiculos disponibles' para ver los que
+              estan disponibles
+            </p>
+          </div>
+          <img
+            className={classes.temporalBg}
+            src="https://i.postimg.cc/SKj4CK6S/semi-front.webp"
+            alt=""
+          />
+        </div>
       )}
 
-      <Modal size={1500} opened={opened} onClose={close} title={selectedCar?.model}>
+      <Modal
+        size={1500}
+        opened={opened}
+        onClose={close}
+        title={selectedCar?.model}
+        styles={{ inner: { alignItems: 'center' } }}
+      >
         {/* Display the selected car details here */}
         <div className={classes.modalImageCont}>
-          {' '}
-          <img src={selectedCar?.images[0]} alt="" />{' '}
-          <img src={selectedCar?.images[1]} alt="" />{' '}
-          <img src={selectedCar?.images[2]} alt="" />{' '}
+          <img src={selectedCar?.images[0]} alt="" />
+          <img src={selectedCar?.images[1]} alt="" />
+          <img src={selectedCar?.images[2]} alt="" />
         </div>
         <p className={classes.modalText}>{selectedCar?.description}</p>
         <div className={classes.modalInfoCont}>
-          <p className={classes.modalInfoT}> <span> Precio: </span>  ${selectedCar.price.toLocaleString()}</p>
-          <p className={classes.modalInfoT}> <span> Modelo:  </span> {selectedCar.year}</p>
-          <p className={classes.modalInfoT}> <span> Color:  </span> {selectedCar.color}</p>
+          <p className={classes.modalInfoT}>
+            {' '}
+            <span className={classes.modalSpam}> Precio: </span> $
+            {selectedCar?.price.toLocaleString()}
+          </p>
+          <p className={classes.modalInfoT}>
+            {' '}
+            <span className={classes.modalSpam}> Modelo: </span> {selectedCar?.year}
+          </p>
+          <p className={classes.modalInfoT}>
+            {' '}
+            <span className={classes.modalSpam}> Color: </span> {selectedCar?.color}
+          </p>
         </div>
         {/* You can add more details about the selected car here */}
       </Modal>
